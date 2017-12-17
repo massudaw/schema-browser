a1f77cfb4660a0282fee607441bc2daf58ddbbb7
Wesley Sales Massuda <wesley.massuda@gmail.com>
2017-12-17 17:31:08 -0200

    update function

---
+++ browser/Postgresql/Function.hs
@@ -0,0 +1,33 @@
+module Postgresql.Function (replaceexpr,indexLabel) where
+
+import Types
+import Step.Host
+import Data.Monoid
+import Data.Text (Text)
+import qualified Data.Text as T
+
+replaceexpr :: Expr -> [Text]  -> Text
+replaceexpr k ac = go k
+  where
+    go :: Expr -> Text
+    go (Function i e) = i <> "(" <> T.intercalate ","  (go   <$> e) <> ")"
+    go (Value i ) = (ac !! i )
+
+indexLabel  :: Show a =>
+    Access Key
+    -> TBData Key a
+    -> (Column  Key a)
+indexLabel p@(IProd b l) v =
+    case findAttr l v of
+      Just i -> i
+      Nothing -> error "no fk"
+indexLabel p@(Nested l m) v =
+  case findFK (iprodRef <$> l) v of
+    Just (IT k (TB1 a)) -> indexLabelU m a
+    Just (IT k (LeftTB1 (Just (TB1 a)))) -> indexLabelU m a
+    Nothing -> error "no fk"
+
+indexLabel  i v = error (show (i, v))
+indexLabelU  (Many [One nt]) v = indexLabel  nt v
+
+
